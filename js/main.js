5131a186628deff1449e3ca92f4cd10bd9e80be1
P4anix <pancok_1@yahoo.it>
2023-02-22 16:01:28 +0100

    milestone 1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 'use strict'
 
 /* variabili utenti */
-const km = Number(prompt("Inserisci il numero di chilometri da percorrere: "));
-const eta = Number(prompt("Inserisci l'età del passeggero: "));
+const km = Number(document.getElementById("km").value);
+const eta = Number(document.getElementById("eta").value);
 
 /* calcolo del prezzo del biglietto in base ai km */
 let prezzoIntero = km * 0.21;
@@ -16,6 +16,14 @@ if (eta < 18) {
 } else {
     prezzoFinale = prezzoIntero;
 }
-
 /* visualizzazione del prezzo finale */
 console.log("Il prezzo del biglietto è di " + prezzoFinale.toFixed(2) + " euro.");
+
+const inputButton = document.getElementById('prezzo');
+
+inputButton.addEventListener('click', 
+    
+    function() {
+        document.querySelector('h1').innerHTML = `${prezzoFinale.toFixed(2)} €`
+    }
+)
