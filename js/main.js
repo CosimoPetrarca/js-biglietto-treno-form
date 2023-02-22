f884f24ac2235852d1b8b9125de7da5f6a452066
P4anix <pancok_1@yahoo.it>
2023-02-22 17:04:41 +0100

    first step

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 'use strict'
 
 /* variabili utenti */
-const km = Number(document.getElementById("km").value);
-const eta = Number(document.getElementById("eta").value);
+let nome = document.getElementById("nome").value;
+let km = document.getElementById("km").value;
+let eta = document.getElementById("eta").value;
 
 /* calcolo del prezzo del biglietto in base ai km */
 let prezzoIntero = km * 0.21;
 
  /* applicazione degli sconti in base all'età del passeggero */
 let prezzoFinale;
-if (eta < 18) {  
+if (eta === 'minorenni') {  
     prezzoFinale = prezzoIntero * 0.8;
-} else if (eta >= 65) { 
+} else if (eta === 'over65') { 
     prezzoFinale = prezzoIntero * 0.6;
 } else {
     prezzoFinale = prezzoIntero;
@@ -24,6 +25,6 @@ const inputButton = document.getElementById('prezzo');
 inputButton.addEventListener('click', 
     
     function() {
-        document.querySelector('h1').innerHTML = `${prezzoFinale.toFixed(2)} €`
+        document.querySelector('h2').innerHTML = `${prezzoFinale.toFixed(2)} €`
     }
 )
